782c96eeaec520e6dfcb498722910d11f9e49752
mennamohamed2976 <menna.8762004@gmail.com>
2026-06-09 22:43:05 +0300

    Update models.py

--- core/models.py
+++ core/models.py
@@ -700,7 +700,7 @@ class MRIReport(models.Model):
 
     before_scan = models.FileField(upload_to='mri/before/', null=True, blank=True)
     after_scan = models.FileField(upload_to='mri/after/', null=True, blank=True)
-    npl report = models.FileField(upload_to='mri/npl/', null=True, blank=True)
+    npl_report = models.FileField(upload_to='mri/npl/', null=True, blank=True)
     
     ai_result = models.TextField(null=True, blank=True)
     mismatch_alert = models.BooleanField(default=False)
